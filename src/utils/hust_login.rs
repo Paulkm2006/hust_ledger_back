baeb1e4e4da7d12d02384dfa02f534768760dba1
Your Name <you@example.com>
2024-11-10 22:13:24 +0800

    feat(auth): login API

---
+++ src/utils/hust_login.rs
@@ -0,0 +1,8 @@
+		.default_headers(headers!{
+			"User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+			"Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+			"Accept-Encoding" => "gzip, deflate, sdch",
+			"Accept-Language" => "zh-CN,zh;q=0.8",
+			"Connection" => "keep-alive",
+		})
+			"Accept-Language" => "zh-CN,zh;q=0.8",
