1b0dcc55eb55281a1324f47863ceb9e6667499f6
Paul Liu <20290410+Paulkm2006@users.noreply.github.com>
2024-11-19 21:16:45 +0800

    refactor(login): remove jsession

--- src/router/router.rs
+++ src/router/router.rs
@@ -6,8 +6,7 @@ pub fn config(cfg: &mut web::ServiceConfig) {
 	cfg.service(
 	web::scope("/login")
 			.route("/captcha", web::get().to(utils::captcha::get_captcha))
-			.route("", web::post().to(utils::hust_login::login))
-			.route("/refresh", web::get().to(utils::hust_login::refresh_jsession)));
+			.route("", web::post().to(utils::hust_login::login)));
 	cfg.service(
 		web::scope("/report").route("", web::get().to(controller::report::report))
 	);
--- src/utils/hust_login.rs
+++ src/utils/hust_login.rs
@@ -25,7 +25,6 @@ struct Info{
 	status: i32,
 	msg: String,
 	castgc: String,
-	jsessionid: String,
 }
 
 pub async fn get_account_no(jsession: &str) -> Result<String, Box<dyn std::error::Error>>{
@@ -84,24 +83,12 @@ pub async fn login(cred: web::Json<Credential>) -> Result<impl Responder, Box<dy
 			status: 403,
 			msg: format!("Failed to login: {}", e),
 			castgc: "".to_string(),
-			jsessionid: "".to_string(),
 		})),
 	};
 	Ok(HttpResponse::Ok().json(Info{
 		status: 200,
 		msg: "Login successful.".to_string(),
 		castgc: castgc,
-		jsessionid: "".to_string()
-	}))
-}
-
-pub async fn refresh_jsession(castgc: web::Query<CASRefresh>) -> Result<impl Responder, Box<dyn std::error::Error>> {
-	let jsession = get_jsession(&castgc.castgc).await?;
-	Ok(HttpResponse::Ok().json(Info{
-		status: 200,
-		msg: "JSESSIONID refreshed.".to_string(),
-		castgc: castgc.castgc.clone(),
-		jsessionid: jsession,
 	}))
 }
 
@@ -134,36 +121,3 @@ async fn get_castgc(cred: web::Json<Credential>, client: &mut Client) -> Result<
 
 }
 
-async fn get_jsession(castgc: &String) -> Result<String, Box<dyn std::error::Error>> {
-	let url = "http://ecard.m.hust.edu.cn/wechat-web/QueryController/Queryurl.html";
-	let re_jsession = regex::Regex::new(r#"jsessionid=(.*)"#).unwrap();
-
-	let cookie_store = reqwest::cookie::Jar::default();
-	let url_token = reqwest::Url::parse("https://pass.hust.edu.cn").unwrap();
-	cookie_store.add_cookie_str(("CASTGC=".to_owned() + &castgc).as_str(), &url_token);
-
-	let client = Client::builder()
-	.cookie_provider(cookie_store.into())
-	.default_headers({
-		let mut headers = header::HeaderMap::new();
-		headers.insert(header::USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3".parse().unwrap());
-		headers.insert(header::ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8".parse().unwrap());
-		headers.insert(header::ACCEPT_ENCODING, "gzip, deflate, sdch".parse().unwrap());
-		headers.insert(header::ACCEPT_LANGUAGE, "zh-CN,zh;q=0.8".parse().unwrap());
-		headers.insert(header::CONNECTION, "keep-alive".parse().unwrap());
-		headers
-	})
-	.build()
-	.unwrap();
-
-	let res = client.get(url).send().await?;
-	let jsession = match re_jsession.captures(res.url().as_str()){
-		Some(caps) => match caps.get(1){
-			Some(cap) => cap.as_str(),
-			None => {return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "String match failed")));},
-		},
-		None => {return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Regex match failed. Consider refreshing the captcha.")));},
-	};
-
-	Ok(jsession.to_string())
-}
